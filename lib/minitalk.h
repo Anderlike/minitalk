c63d9fa3112f1c91525e332514119d02b46aea62
Pio <77726736+Anderlike@users.noreply.github.com>
2025-02-17 18:21:34 +0000

    Initial commit.
    Messed around with getpid and signal handlers

---
+++ lib/minitalk.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaleixo- <aaleixo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/17 14:49:06 by aaleixo-          #+#    #+#             */
+/*   Updated: 2025/02/17 14:53:46 by aaleixo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "./ft_printf/Libft/libft.h"
+# include "./ft_printf/ft_printf.h"
+
+#endif
